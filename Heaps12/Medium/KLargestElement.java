98995b4b0366758109f93b5a2fd444ee1b500899
manu <manukhurana10397@gmail.com>
2024-10-02 19:22:44 +0530

    heap

---
+++ Heaps12/Medium/KLargestElement.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class KLargestElement{
+
+	public static List<Integer> getkLargestElement(int[] arr, int k){
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+		for(int i: arr){
+			queue.add(i);
+
+			if(k<queue.size()) queue.poll();
+		}
+
+		List<Integer> result = new ArrayList<>();
+		while(!queue.isEmpty()) result.add(queue.poll());
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {6,5,3,2,8,10,9};
+		System.out.println(getkLargestElement(arr, 4));
+	}
+
+}
