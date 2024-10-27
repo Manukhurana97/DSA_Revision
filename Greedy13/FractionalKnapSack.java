8d115e6ab51fe4b789fe21697c120d8724a54286
manu <manukhurana10397@gmail.com>
2024-10-27 15:47:49 +0530

    'heap'

---
+++ Greedy13/FractionalKnapSack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Item{
+	int weight;
+	int profit;
+
+	Item(int profit, int weight){
+		this.weight = weight;
+		this.profit = profit;
+	}
+}
+
+public class FractionalKnapSack{
+	static double fractionalKnapsack(int W, Item arr[], int n) {
+
+		if(n == 0) return 0;
+
+		PriorityQueue<Item> queue = new PriorityQueue<>((b, a) -> Double.compare((double) a.profit / a.weight, (double)b.profit / b.weight));
+
+		double profit = 0;
+
+		for(Item i: arr) queue.add(i);
+
+		while(!queue.isEmpty() || W>0){
+			var current = queue.poll();
+
+			if(current.weight > W){
+				profit += ((double)current.profit / current.weight) * W;
+				break;
+			}
+
+			profit+=current.profit;
+			W -= current.weight;
+		}
+		return profit;
+	}
+
+	public static void main(String[] args) {
+		Item[] arr = {new Item(100, 20), new Item(60, 10), new Item(100, 50), new Item(200, 50)};
+		System.out.println(fractionalKnapsack(90, arr, arr.length));
+
+		Item arr1[] = {new Item (100,20),new Item(60,10),new Item(120,30)};
+		System.out.println(fractionalKnapsack(50, arr1, arr1.length));
+	}
+}
--- Greedy13/MinimumPlatforms.java
+++ Greedy13/MinimumPlatforms.java
@@ -3,10 +3,13 @@ import java.util.*;
 class Node{
 	int ind;
 	int time;
+	boolean isArrived;
 
-	Node(int ind, int time){
+	Node(int ind, int time, boolean isArrived){
 		this.ind = ind;
 		this.time = time;
+		this.isArrived = isArrived;
+
 	}
 }
 
@@ -35,26 +38,26 @@ public class MinimumPlatforms{
 
 
     static int findPlatform1(int arr[], int dep[]) {
-    	Map<Integer, Node> map = new HashMap<>();
     	PriorityQueue<Node> queue = new PriorityQueue<>((a, b) -> a.time - b.time);
 
     	for(int i=0;i<arr.length;i++){
-    		queue.add(new Node(i, arr[i]));
-    		queue.add(new Node(i, arr[i]));
+    		queue.add(new Node(i, arr[i], true));
+    		queue.add(new Node(i, dep[i], false));
     	}
 
     	int maxPlatformRequired = 1;
+    	int currentTrains = 0;
 
     	while(!queue.isEmpty()){
     		var currentNode = queue.poll();
 
-    		if(map.containsKeys(currentNode.ind)){
-    			map.remove(currentNode.ind);
-    		}else{
-    			map.put(i, currentNode);
+    		if(currentNode.isArrived){ 
+    			currentTrains++;
+    			maxPlatformRequired = Math.max(maxPlatformRequired, currentTrains);
     		}
+    		else currentTrains--;
 
-    		maxPlatformRequired = Math.max(maxPlatformRequired, map.size());
+    		
     	}
 
     	return maxPlatformRequired;
@@ -64,4 +67,8 @@ public class MinimumPlatforms{
 
 // [0900, 0940, 0950, 1100, 1500, 1800], 
 // [0910, 1200, 1120, 1130, 1900, 2000]
+
+// 1:0900, 1:0910, 2:0940, 3:0950, 4:1100, 3:1120 4:1130 2:1200 5:1500 6:1800 5:1900 6:2000
+// 1:0900, 1:0910
+//  2:0940, 3:0950, 4:1100,
 
