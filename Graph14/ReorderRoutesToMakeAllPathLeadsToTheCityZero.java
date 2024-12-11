c2b94764fdd506ab2b1a9c7767a2051a3e427166
manu <manukhurana10397@gmail.com>
2024-12-12 00:41:29 +0530

    graph, REORDER ROUTES TO MAKE ALL PATHS LEAD TO THE CITY ZERO

--- Graph14/ReorderRoutesToMakeAllPathLeadsToTheCityZero.java
+++ Graph14/ReorderRoutesToMakeAllPathLeadsToTheCityZero.java
@@ -7,10 +7,13 @@ public class ReorderRoutesToMakeAllPathLeadsToTheCityZero{
             adjList.add(new ArrayList<>());
         }
 
+        // create a 2 way connection
         for(int[] connection: connections){
             adjList.get(connection[0]).add(new int[]{connection[1], 1}); // 1 indicates road points away from city 0
             adjList.get(connection[1]).add(new int[]{connection[0], 0});  // 0 indicates road points towards city 0
         }
+        // if there is not from (eg 0 -> 1) ,this mean is away from 0, we will take 1 
+        // if there is not from (eg 1 -> 0) ,this mean is going twoards 0, but since 0 is initiali it will be alreeady visited we will not count this  
 
         int[] visited = new int[n];
         return dfs(0, adjList, visited);
