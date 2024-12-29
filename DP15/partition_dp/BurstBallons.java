a3021e00fd7a03c9bf204d24c11e065baec57c51
manu <manukhurana10397@gmail.com>
2024-12-30 00:58:38 +0530

    DP, Parition dp, BurstBallons

---
+++ DP15/partition_dp/BurstBallons.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class BurstBallons{
+
+	private int maxCoins(int[] arr){
+		int n = arr.length;
+		List<Integer> list = new ArrayList<>();
+
+		list.add(1);
+		for(int i: arr) list.add(i);
+		list.add(1);
+
+		// return recursion(1, n, list);
+
+		int[][] dp = new int[n+2][n+2];
+		// return memoization(1, n, list, dp);
+
+		return tabulation(n, list, dp);
+
+	}
+
+
+	private int recursion(int i, int j, List<Integer> arr){
+		if(i>j) return 0;
+
+		int maxProfit = 0;
+		for(int ind = i; ind<=j; ind++){
+			int cost = arr.get(i-1) * arr.get(ind) * arr.get(j+1) + recursion(i, ind-1, arr) + recursion(ind+1, j, arr);
+			maxProfit = Math.max(maxProfit, cost);
+		}
+
+		return maxProfit;
+	}
+
+
+	private int memoization(int i, int j, List<Integer> arr, int[][] dp){
+		if(i>j) return 0;
+
+		if(dp[i][j] != 0) return dp[i][j];
+
+		int maxProfit = 0;
+		for(int ind = i; ind<=j; ind++){
+			int cost = arr.get(i-1) * arr.get(ind) * arr.get(j+1) + memoization(i, ind-1, arr, dp) + memoization(ind+1, j, arr, dp);
+			maxProfit = Math.max(maxProfit, cost);
+		}
+
+
+		dp[i][j] = maxProfit;
+		return maxProfit;
+	}
+
+
+
+	private int tabulation(int n, List<Integer> arr, int[][] dp){
+
+		for(int i=n; i>=1; i--){
+			for(int j=0; j<=n; j++){
+				
+				if(i>j) continue;
+
+				int maxProfit = 0;
+				for(int ind = i; ind<=j; ind++){
+					int cost = arr.get(i-1) * arr.get(ind) * arr.get(j+1) + dp[i][ind-1] + dp[ind+1][j];
+					maxProfit = Math.max(maxProfit, cost);
+				}
+
+				dp[i][j] = maxProfit;
+			}
+		}
+		return dp[1][n];
+	}
+
+	
+	public static void main(String[] args) {
+		BurstBallons obj = new BurstBallons();
+		int[] arr = {3, 1, 5, 8};
+		System.out.println(obj.maxCoins(arr));
+	}
+}
