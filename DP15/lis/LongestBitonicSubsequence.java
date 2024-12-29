5b04446f3f87980eacfcad22da70ccd9675a091c
manu <manukhurana10397@gmail.com>
2024-12-29 13:25:17 +0530

    Dp, LIS

---
+++ DP15/lis/LongestBitonicSubsequence.java
@@ -0,0 +1,58 @@
+// bitonic : increasing, decreasing, increasing & decreasing
+
+import java.util.*;
+
+public class LongestBitonicSubsequence{
+
+	private int getLongestSequence(int[] arr){
+
+		int n = arr.length;
+		int[] dp1 = tabulation1(arr);
+		int[] dp2 =	tabulation2(arr);
+
+		int max = 0;
+		for(int i=0; i<n; i++){
+			max = Math.max(max, dp1[i] + dp2[i] - 1);
+		}
+
+		return max;
+	}
+
+	
+	private int[] tabulation1(int[] arr){
+		
+		int[] dp = new int[arr.length+1];
+		Arrays.fill(dp, 1);
+
+		for(int index=0; index<arr.length; index++){
+			for(int prev = 0; prev<index;prev++){
+				if(arr[prev] < arr[index] && dp[index]<dp[prev]+1){
+					dp[index] = dp[prev]+1;
+				}
+			}
+		}
+		return dp;
+	}
+
+	private int[] tabulation2(int[] arr){
+		
+		int[] dp = new int[arr.length+1];
+		Arrays.fill(dp, 1);
+
+		for (int index = arr.length - 2; index >= 0; index--) {
+            for (int prev = arr.length - 1; prev > index; prev--) {
+				if(arr[prev] < arr[index] && dp[index]<dp[prev]+1){
+					dp[index] = dp[prev]+1;
+				}
+			}
+		}
+		return dp;
+	}
+
+
+	public static void main(String[] args) {
+		LongestBitonicSubsequence obj = new LongestBitonicSubsequence();
+		int[] arr = {1, 11, 2, 10, 4, 5, 2, 1};
+		System.out.println(obj.getLongestSequence(arr));
+	}
+}
---
+++ DP15/lis/LongestStringChain.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/*similar to PrintLongestIncreasingSubSequence*/
+public class LongestStringChain{
+
+	public int longestChain(String[] arr){
+
+		int n = arr.length, maxLen = 0;
+		int[] dp = new int[n];
+		Arrays.fill(dp, 1);
+
+		for(int i=0; i<arr.length; i++){
+			for(int prev = 0; prev<=i; prev++){
+				if(compare(arr[i], arr[prev]) && dp[i] < dp[prev] + 1){
+					dp[i] = dp[prev] + 1;
+				}
+			}
+
+			if(dp[i]>maxLen){
+				maxLen = dp[i];
+			}
+		}
+
+		return maxLen;
+	}
+
+
+	private boolean compare(String a, String b){
+		if(a.length() != b.length() + 1) return false;
+
+
+		int i=0, j=0;
+
+		while(i<a.length() && i<b.length()){
+			if(a.charAt(i) != b.charAt(j)){
+				j+=1;
+			}
+			i+=1;
+		}
+
+		return j == b.length();
+
+	}
+
+	public static void main(String[] args) {
+		LongestStringChain obj = new LongestStringChain();
+		String[] arr = {"a", "b", "c", "ab", "ac", "bc", "abc"};
+		System.out.println(obj.longestChain(arr));
+	}
+}
