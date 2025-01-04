3c2c67707223fa983be8bc881772875ef81eb85b
manu <manukhurana10397@gmail.com>
2025-01-04 22:53:30 +0530

    dp, neetcode

---
+++ DP15/neetcode/MaximalSquare.java
@@ -0,0 +1,106 @@
+public class MaximalSquare{
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length;
+        if(matrix == null || rows == 0 || cols == 0) return 0;
+         
+        Integer[][] memo = new Integer[rows+1][cols+1];
+
+        int maxValue = 0;
+        // for(int r=0; r<rows; r++){
+        //     for(int c=0; c<cols; c++){
+        //         // maxValue = Math.max(maxValue, recursion(r, c, matrix));
+
+        //         maxValue = Math.max(maxValue, memoization(r, c, matrix, memo));
+        //     }
+        // } 
+         
+        // return maxValue * maxValue;
+
+        // return tabulation(matrix);
+
+        return spaceOptimization(matrix);
+    }
+
+    private int recursion(int r, int c, char[][] matrix){
+        if(r < 0 || c < 0 || matrix[r][c] == '0') return 0;
+
+        
+        int left = recursion(r-1, c, matrix);
+        int top = recursion(r, c-1, matrix);
+        int topleft = recursion(r-1, c-1, matrix);
+
+        return 1 + Math.min(Math.min(left, top), topleft);
+
+    }
+
+
+    private int memoization(int r, int c, char[][] matrix, Integer[][] dp){
+        if(r < 0 || c < 0 || matrix[r][c] == '0') return 0;
+
+        if(dp[r][c] != null) return dp[r][c];
+        
+        int left = memoization(r-1, c, matrix, dp);
+        int top = memoization(r, c-1, matrix, dp);
+        int topleft = memoization(r-1, c-1, matrix, dp);
+
+        return dp[r][c] = 1 + Math.min(Math.min(left, top), topleft);
+
+    }
+
+
+    private int tabulation(char[][] matrix){
+        int maxValue = 0;
+        int rows = matrix.length, cols = matrix[0].length;
+        int[][] dp = new int[rows+1][cols+1];
+        
+        for(int r=0; r<rows; r++){
+            dp[r][0] = matrix[r][0] == '0' ? 0 : 1;
+            maxValue = Math.max(maxValue, dp[r][0]);
+        }
+        for(int c=0; c<cols; c++){
+            dp[0][c] = matrix[0][c] == '0' ? 0 : 1;
+            maxValue = Math.max(maxValue, dp[0][c]);
+        }
+
+        for(int r=0; r<rows; r++){
+            for(int c=1; c<cols; c++){
+                if(matrix[r][c] == '1'){
+                    int left = dp[r-1][c];
+                    int top = dp[r][c-1];
+                    int topleft = dp[r-1][c-1];
+
+                    dp[r][c] = 1 + Math.min(Math.min(left, top), topleft);
+                    maxValue = Math.max(maxValue, dp[r][c]);
+                }else{
+                    dp[r][c] = 0;
+                }
+            }
+        }
+
+        return maxValue * maxValue;
+    }
+
+
+    private int spaceOptimization(char[][] matrix) {
+        int maxValue = 0;
+        int rows = matrix.length, cols = matrix[0].length;
+
+        int[] prev = new int[cols + 1]; 
+        int[] curr = new int[cols + 1]; 
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 1; c <= cols; c++) { 
+                if (matrix[r][c - 1] == '1') {
+                    curr[c] = 1 + Math.min(Math.min(prev[c], curr[c - 1]), prev[c - 1]);
+                    maxValue = Math.max(maxValue, curr[c]);
+                } else {
+                    curr[c] = 0;
+                }
+            }
+            prev = curr.clone();
+            
+        }
+
+        return maxValue * maxValue;
+    }
+}
---
+++ DP15/neetcode/Triangle.java
@@ -0,0 +1,82 @@
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+        // return recursion(0, 0, triangle);
+        int[][] dp = new int[triangle.size()+1][triangle.get(triangle.size()-1).size()+1];
+        // return memoization(0, 0, triangle, dp);
+
+        // return tabulation(triangle, dp);
+
+        return spaceOptimization(triangle);
+    }
+    
+
+    public int recursion(int r, int c, List<List<Integer>> triangle) {
+
+        if(c >= triangle.get(r).size()) return Integer.MAX_VALUE;
+        if(r == triangle.size()-1) return triangle.get(r).get(c);
+        
+        int down = recursion(r+1, c, triangle);
+        int downLeft = recursion(r+1, c+1, triangle);
+
+        return triangle.get(r).get(c) + Math.min(down, downLeft);
+    }
+
+
+    public int memoization(int r, int c, List<List<Integer>> triangle, int[][] dp) {
+
+        if(c >= triangle.get(r).size()) return Integer.MAX_VALUE;
+        if(r == triangle.size()-1) return triangle.get(r).get(c);
+
+        if(dp[r][c] != 0) return dp[r][c];
+        
+        int down = memoization(r+1, c, triangle, dp);
+        int downLeft = memoization(r+1, c+1, triangle, dp);
+
+        return dp[r][c] = triangle.get(r).get(c) + Math.min(down, downLeft);
+    }
+
+
+    public int tabulation( List<List<Integer>> triangle, int[][] dp) {
+        int rows = triangle.size(), cols = triangle.get(triangle.size()-1).size();
+
+        for(int c=0; c<cols; c++){
+            dp[rows-1][c] = triangle.get(rows-1).get(c);
+        }
+        
+        for(int r=rows-2; r>=0; r--){
+            for(int c=triangle.get(r).size()-1; c>=0; c--){
+                int down = dp[r+1][c];
+                int downLeft = c+1 > triangle.get(r).size() ? Integer.MAX_VALUE :  dp[r+1][c+1];
+
+                dp[r][c] = triangle.get(r).get(c) + Math.min(down, downLeft);
+            }
+        }        
+        
+        return dp[0][0];
+    }
+
+
+    public int spaceOptimization( List<List<Integer>> triangle) {
+        int rows = triangle.size(), cols = triangle.get(triangle.size()-1).size();
+
+        int[] prev = new int[cols+1];
+
+        for(int c=0; c<cols; c++){
+            prev[c] = triangle.get(rows-1).get(c);
+        }
+        
+        for(int r=rows-2; r>=0; r--){
+            int[] curr = new int[cols+1];
+            for(int c=triangle.get(r).size()-1; c>=0; c--){
+                int down = prev[c];
+                int downLeft = c+1 > triangle.get(r).size() ? Integer.MAX_VALUE :  prev[c+1];
+
+                curr[c] = triangle.get(r).get(c) + Math.min(down, downLeft);
+            }
+            prev = curr;
+        }        
+        
+        return prev[0];
+    }
+}
