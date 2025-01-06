7d6d757cf70af24bbd92c19ae676c183ee32bad2
manu <manukhurana10397@gmail.com>
2025-01-07 01:27:27 +0530

    dp, neetcode

---
+++ DP15/neetcode/CountingBits.java
@@ -0,0 +1,18 @@
+public class CountingBits{
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+
+        for(int i=1; i<=n; i++){
+            int count = 0 ;
+            int j = i;
+            while(j>0){
+                count += j&1;
+                j>>>=1;
+            }
+
+            result[i] += count;
+        }
+
+        return result;
+    }
+}
