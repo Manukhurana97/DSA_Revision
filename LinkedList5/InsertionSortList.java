f470405d258b47218350acd0f6fe29b61fa20bb2
manu <manukhurana10397@gmail.com>
2025-04-27 01:30:07 +0530

    InsertionSortList

--- LinkedList5/InsertionSortList.java
+++ LinkedList5/InsertionSortList.java
@@ -1,5 +1,40 @@
 // https://leetcode.com/problems/insertion-sort-list/
 
 public class InsertionSortList {
-	
+	public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode current = head.next;
+        head.next = null;
+
+        while(current != null) {
+            ListNode prev = null, temp = dummy.next, next = current.next;
+
+            while(temp != null && temp.val<current.val){
+                prev = temp;
+                temp = temp.next;
+            }
+
+            if(temp != null && temp.val >= current.val) {
+                if(prev == null){ // insert into head
+                    current.next = temp;
+                    dummy.next = current;   
+                }else{
+                    current.next = temp;
+                    prev.next = current;
+                }
+            }else{
+                head.next = current;
+                head = current;
+                head.next = null;
+            }
+            
+            current = next;
+        }
+
+
+        return dummy.next;
+    }
 }
