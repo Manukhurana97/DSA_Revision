de409a88e249019eeedf9efd56a48931a63a066a
manu <manukhurana10397@gmail.com>
2025-04-27 01:53:48 +0530

    Insertion Sort List

--- LinkedList5/InsertionSortList.java
+++ LinkedList5/InsertionSortList.java
@@ -37,4 +37,34 @@ public class InsertionSortList {
 
         return dummy.next;
     }
+
+
+
+    // -----------------------------------------------------------------------------------------
+
+
+    ublic ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode dummy = new ListNode(0);
+        ListNode current = head;
+
+       while(current != null){
+        ListNode prev = dummy;
+        ListNode next = current.next;
+
+        while(prev.next != null && prev.next.val <current.val){
+            prev = prev.next;
+        }
+
+        current.next = prev.next;
+        prev.next = current;
+
+        current = next;
+       }
+
+
+        return dummy.next;
+    }
+
 }
