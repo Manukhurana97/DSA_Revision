323f6572ec0be1973515a1def787c0d2b1b34680
manu <manukhurana10397@gmail.com>
2025-06-09 23:54:54 +0530

    tree

--- Tree10/ArrayRepresentationOfHeap.java
+++ Tree10/ArrayRepresentationOfHeap.java
@@ -11,4 +11,13 @@ public class ArrayRepresentationOfHeap{
         
         return true;
     }
+
+    public static void main(String[] args) {
+        ArrayRepresentationOfHeap obj = new ArrayRepresentationOfHeap();
+        long[] arr = {5,4,3,2,1};
+        System.out.println(obj.countSub(arr, arr.length));
+        
+        long[] arr1 = {1,2,3,4,5};
+        System.out.println(obj.countSub(arr1, arr1.length));
+    }
 }
