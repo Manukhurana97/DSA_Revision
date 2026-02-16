8aea6a47fcb70ea84bc82d2c10ccceb335e16671
manu khurana <manukhurana10397@gmail.com>
2026-02-17 00:55:14 +0530

    2026-02-17

--- Recursion6/IntegerToRoman.java
+++ Recursion6/IntegerToRoman.java
@@ -1,10 +1,10 @@
 // https://leetcode.com/problems/integer-to-roman/
 
 public class IntegerToRoman {
-	String[] once = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+	String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
     String[] tens = {"", "X", "XX", "XXX", "XL", "L","LX","LXX","LXXX","XC"};
-    String[] hundres = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String[] thousand = {"", "M", "MM", "MMM"};
+    String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String[] thousands = {"", "M", "MM", "MMM"};
 
     public String intToRoman(int num) {
        
@@ -13,15 +13,15 @@ public class IntegerToRoman {
 
     public String helper(int num) {
         if(num < 10) {
-            return once[num];
+            return tens[num];
         } 
         if(num < 100) {
             return tens[num/10] + helper(num%10);
         } 
         if(num < 1000) { 
-            return hundres[num/100] + helper(num%100);
+            return hundreds[num/100] + helper(num%100);
         }
-        return thousand[num/1000] + helper(num%1000);
+        return thousands[num/1000] + helper(num%1000);
     }
 
 
