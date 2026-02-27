8a30c4f8ec4d6be7dddb557d60df81e36bb9418c
manu khurana <manukhurana10397@gmail.com>
2026-02-27 17:11:58 +0530

    2026-02-27

--- Arrays2/Merge2SortedArrayWithoutExtraSpace.py
+++ Arrays2/Merge2SortedArrayWithoutExtraSpace.py
@@ -88,6 +88,17 @@ def next_gap(l):
 
 
 
+# public void merge(int[] nums1, int m, int[] nums2, int n) {
+#         int i = m-1, j = n-1, k = nums1.length - 1;
+#         while(i >= 0 && j >= 0) {
+#             nums1[k--] = (nums1[i] < nums2[j]) ? nums2[j--] : nums1[i--];
+#         }
+
+#         while(j>=0) {
+#             nums1[k--] = nums2[j--];
+#         }
+#     }
+
 
 # [1,4,8,10], [2,3,9] :- [1,2,8,10], [4,3,9]  :- [1,2,3,10], [4,3,9]
 
